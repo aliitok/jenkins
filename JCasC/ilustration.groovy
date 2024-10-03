2112befc54649d1f514f36f86d9061c990606e83
muhammadriadi <muhammad.riadi@allianz.co.id>
2024-10-04 02:19:21 +0700

    fix the code

--- JCasC/ilustration.groovy
+++ JCasC/ilustration.groovy
@@ -1,30 +1,33 @@
 folder("AZ360-Build-Sales-Tools") {
-    displayName('Az360 Build Sales Tools')
-    description('This folder contains the jobs for Sales tools')}
+    displayName('AZ360 Build Sales Tools')
+    description('This folder contains the jobs for Sales tools')
+}
+
 folder('AZ360-Build-Sales-Tools/az360-illustration-be') {
-    displayName('Az360 Illustration be')
-    description('This folder for illustration be')}
-    
+    displayName('AZ360 Illustration BE')
+    description('This folder for illustration BE')
+}
+
 pipelineJob('AZ360-Build-Sales-Tools/az360-illustration-be/dev') {
-            definition {
-                cpsScm {
-                    scm {
-                        git {
-                            remote {
-                                url('https://github.developer.allianz.io/azap-az360/az360-illustration-be.git') // Replace with your repository URL
-                                credentials('git-token-credentials') // Optional: specify credentials if needed
-                            }
-                            branches('*/main') // Specify the branch containing your Jenkinsfile
-                        }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.developer.allianz.io/azap-az360/az360-illustration-be.git')
+                        credentials('git-token-credentials') // Ensure this credential ID is correct
                     }
-                    scriptPath('Jenkinsfile') // Path to Jenkinsfile in the repository
+                    branches('*/main') // Specify the branch containing your Jenkinsfile
                 }
-            }
-            keepDependencies(false)
-            logRotator {
-                daysToKeep(7)
-                numToKeep(7)
-                artifactDaysToKeep(-1)
-                artifactNumToKeep(-1)
+                scriptPath('Jenkinsfile') // Path to Jenkinsfile in the repository
             }
         }
+        keepDependencies(false)
+        logRotator {
+            daysToKeep(7)
+            numToKeep(7)
+            artifactDaysToKeep(-1)
+            artifactNumToKeep(-1)
+        }
+    }
+}
